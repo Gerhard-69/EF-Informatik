d8e2dbe6d22103e59608ab49dfe4c919c101e275
Linus <linus.arnaschus@edu.gbsl.ch>
2022-09-16 16:32:41 +0200

    super spiel

---
+++ Numtrip/game.py
@@ -0,0 +1,15 @@
+zeilen = 10
+ = 10
+matrix = [
+    [2,3,2,6,5],[6,2,5,7,4],[3,9,5,2,0],[0,6,1,3,4],[2,4,6,8,2]
+]
+def pprint(mat2d):
+    for zeile in mat2d:
+        print(zeile)
+
+for i in range(zeilen):
+    "Spielfeld"
+    for i in range(wert)
+pprint (matrix)
+
+print(+--)
