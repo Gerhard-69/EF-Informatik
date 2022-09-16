7273cf58a7f6ae5b3f9998276b7a6bdbdc1cc056
Linus <linus.arnaschus@edu.gbsl.ch>
2022-09-16 16:36:29 +0200

    vallah krise

--- Numtrip/game.py
+++ Numtrip/game.py
@@ -1,5 +1,4 @@
 zeilen = 10
- = 10
 matrix = [
     [2,3,2,6,5],[6,2,5,7,4],[3,9,5,2,0],[0,6,1,3,4],[2,4,6,8,2]
 ]
@@ -7,9 +6,6 @@ def pprint(mat2d):
     for zeile in mat2d:
         print(zeile)
 
-for i in range(zeilen):
-    "Spielfeld"
-    for i in range(wert)
 pprint (matrix)
 
-print(+--)
+print("+-----+------+------+-----+-----+-----+\n"  "I     I      I      I     I     I     I")
