055651d99a18d72cacb8d4a6063098ba05f61802
Linus <linus.arnaschus@edu.gbsl.ch>
2022-10-21 16:12:52 +0200

    Schritt 4

---
+++ exercises/Hangman Game/Hangman.py
@@ -0,0 +1,49 @@
+# Variablen
+gesucht = 'test'
+
+gefunden = []
+falsch_geraten = []
+
+def show():
+    print('Falsche Buchstaben:', falsch_geraten)
+    for buchstabe in gesucht:
+        if buchstabe in gefunden:
+            print(buchstabe, end=' ')
+    else:
+        print('_', end=' ')
+    print('')
+
+def is_valid(inp):
+    return True
+
+
+def eingabe():
+    buchstabe = input('Buchstabe? ')
+    while not is_valid(buchstabe):
+            buchstabe = input('Buchstabe? ')
+    return buchstabe.lower()
+
+def auswerten(valid_inp):
+    if valid_inp in gesucht:
+        gefunden.append(valid_inp)
+    else:
+        falsch_geraten.append(valid_inp)
+
+def gewonnen():
+    for buchstabe in gesucht:
+        if buchstabe not in gefunden:
+            return False
+    return True
+
+def game_over():
+    pass
+
+def play():
+    
+
+
+
+
+
+
+
