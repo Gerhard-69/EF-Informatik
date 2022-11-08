0400d77d6362218f508388fe4b5e3eb223b237db
Linus <linus.arnaschus@edu.gbsl.ch>
2022-11-08 09:53:08 +0100

    Zahlen seite hinzugefügt

--- Numtrip/game.py
+++ Numtrip/game.py
@@ -5,25 +5,29 @@ board = [
     [2, 8, 1, 4, 1],
     [2, 4, 4, 4, 4]
 ]
-#Zahlen oben
+#Zahlen oben = x
 x = 0
+y = 0
+print ('   ', end='')
 for i in board:
     x = x+1
     print ('', x, end='')
 print (' ')
+print ('   ', end='')
 
-#Zahlen seite
-
+#Zahlen seite = y
 
 for zeile in board:
     for zelle in zeile:
         print(' -', end='')
     print(' ',)
     print (' ', end='')
+    y = y+1
+    print (y, '', end='')
     for zelle in zeile:
         print(f'|{zelle}', end='')
     print('|')
-    print (' ', end='')
+    print ('   ', end='')
 
 for zelle in board[0]:
     print(' -', end='')
