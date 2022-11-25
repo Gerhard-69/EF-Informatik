32a7b221aa15a6f0fcbc5801ad06d22947053356
Linus <linus.arnaschus@edu.gbsl.ch>
2022-11-25 16:36:44 +0100

    fange spielstart und eingabe bei numtrip an

--- docs/Numtrip/game.py
+++ docs/Numtrip/game.py
@@ -5,30 +5,41 @@ board = [
     [2, 8, 1, 4, 1],
     [2, 4, 4, 4, 4]
 ]
-#Zahlen oben = x
-x = 0
-y = 0
-print ('  ', end='')
-for i in board:
-    x = x+1
-    print ('', x, end='')
-print (' ')
-print ('  ', end='')
-
-#Zahlen seite = y
-
-for zeile in board:
-    for zelle in zeile:
-        print(' -', end='')
-    print(' ',)
-    print (' ', end='')
-    y = y+1
-    print (y, end='')
-    for zelle in zeile:
-        print(f'|{zelle}', end='')
-    print('|')
+def spielfeld(): 
+    #Zahlen oben = x
+    x = 0
+    y = 0
+    print ('  ', end='')
+    for i in board:
+        x = x+1
+        print ('', x, end='')
+    print (' ')
     print ('  ', end='')
 
-for zelle in board[0]:
-    print(' -', end='')
-print(' ')
+    #Zahlen seite = y
+
+    for zeile in board:
+        for zelle in zeile:
+            print(' -', end='')
+        print(' ',)
+        print (' ', end='')
+        y = y+1
+        print (y, end='')
+        for zelle in zeile:
+            print(f'|{zelle}', end='')
+        print('|')
+        print ('  ', end='')
+
+    for zelle in board[0]:
+        print(' -', end='')
+    print(' ')
+
+spielfeld()
+
+zeilenauswahl = input('Welches Zeile?')
+spaltenauswahl = input('Welche Spalte?')
+zeilenauswahl = int(zeilenauswahl)
+spaltenauswahl = int(spaltenauswahl)
+board[zeilenauswahl][spaltenauswahl] = ' '
+
+spielfeld()
