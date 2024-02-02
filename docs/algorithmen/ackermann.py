33fe56ada2503b33c778ba99b525f1b0cccc3a44
Linus <linus.arnaschus@edu.gbsl.ch>
2024-02-02 12:11:34 +0100

    add ackermann

---
+++ docs/algorithmen/ackermann.py
@@ -0,0 +1,13 @@
+def  ack(m, n):
+    if m == 0:
+        anw = n+1
+    else:
+        if n == 0:
+            anw = ack(m-1, 1)
+        else:
+            anw = ack(m-1,ack(m,n-1))
+    return anw
+
+m = 2
+n = 2
+print (m, n, ack(m,n))
